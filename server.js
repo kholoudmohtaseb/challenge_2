4f3161463119cc62f27eac33c1ac63ac90038b2e
kholoudmohtaseb <kholoud.almohtseb@gmail.com>
2020-11-03 15:59:43 +0200

    csv server

--- server.js
+++ server.js
@@ -1,2 +1,25 @@
+const http = require('http');
+const express = require('express');
+const path = require('path');
+
+const app = express;
+app.use(express.json());
+app.use(express.static("express"));
+
+app.use('/', function (req, res) {
+    // res.sendFile
+});
 
 
+const server = http.createServer(function (req, res) {
+
+});
+
+server.listen(port, function (error) {
+    if (error) {
+        console.log("there is error");
+    } else {
+        // console.log("server is listining");
+        server.listen(port);
+    }
+})
