51cc2800c6fedfd1dd7fb9a180619dd3252a8578
kholoudmohtaseb <kholoud.almohtseb@gmail.com>
2020-11-03 15:01:01 +0200

    csv

--- client/app.js
+++ client/app.js
@@ -1,2 +1,14 @@
+const express = require('express');
+const app = express;
 
+app.get('/', function (req, res) {
+}
+);
 
+app.listen(3000, function () {
+    console.log('server started');
+})
+
+app.post('/', function (req, res) {
+
+})
